11d36d9b5cbb676ca993a17caf600827b98a53d8
Diego Ortiz <diego@innova3x.com>
2021-09-30 01:02:30 -0300

    view

--- fabTransporte/app/views.py
+++ fabTransporte/app/views.py
@@ -104,12 +104,12 @@ appbuilder.add_view(
     ServicioTecnicoView, "ServicioTecnico", icon="fa-folder-open-o", category= "ServicioTecnico"
 
 )
-appbuilder.add_view(
-    FormaDePagoView, "Forma_de_pago", icon="fa-folder-open-o", category="ServicioTecnico"
-)
 
 appbuilder.add_view(
     LocalidadView, "Localidad", icon="fa-folder-open-o", category="Clientes"
+)
+appbuilder.add_view(
+    FormaDePagoView, "Forma_de_pago", icon="fa-folder-open-o", category="ServicioTecnico"
 )
 
 
